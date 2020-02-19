f9b89ecbacd91ec301d5a5929d210c78e0d9cf85
Narender Singh <narender2031@gmail.com>
2020-02-19 13:09:02 +0530

    Fix EventsRegistry.registerNotificationOpened typing

--- lib/src/adapters/CompletionCallbackWrapper.ts
+++ lib/src/adapters/CompletionCallbackWrapper.ts
@@ -1,53 +1,59 @@
 import { NativeCommandsSender } from './NativeCommandsSender';
 import { NotificationCompletion } from '../interfaces/NotificationCompletion';
 import { Platform, AppState } from 'react-native';
-import {NotificationIOS} from "../DTO/NotificationIOS";
-import {Notification} from "..";
+import { NotificationIOS } from '../DTO/NotificationIOS';
+import { Notification } from '..';
+import { NotificationActionResponse } from '../interfaces/NotificationActionResponse';
 
 export class CompletionCallbackWrapper {
-  constructor(
-    private readonly nativeCommandsSender: NativeCommandsSender
-  ) {}
+	constructor(private readonly nativeCommandsSender: NativeCommandsSender) {}
 
-  public wrapReceivedBackgroundCallback(callback: Function): (notification: Notification) => void {
-    return (notification) => {
-      if (!this.applicationIsVisible()) {
-        this.wrapReceivedAndInvoke(callback, notification);
-      }
-    }
-  }
+	public wrapReceivedBackgroundCallback(callback: Function): (notification: Notification) => void {
+		return (notification) => {
+			if (!this.applicationIsVisible()) {
+				this.wrapReceivedAndInvoke(callback, notification);
+			}
+		};
+	}
 
-  public wrapReceivedForegroundCallback(callback: Function): (notification: Notification) => void {
-    return (notification) => {
-      if (this.applicationIsVisible()) {
-        this.wrapReceivedAndInvoke(callback, notification);
-      }
-    }
-  }
+	public wrapReceivedForegroundCallback(callback: Function): (notification: Notification) => void {
+		return (notification) => {
+			if (this.applicationIsVisible()) {
+				this.wrapReceivedAndInvoke(callback, notification);
+			}
+		};
+	}
 
-  private wrapReceivedAndInvoke(callback: Function, notification: Notification) {
-    const completion = (response: NotificationCompletion) => {
-      if (Platform.OS === 'ios') {
-        this.nativeCommandsSender.finishPresentingNotification((notification as unknown as NotificationIOS).identifier, response);
-      }
-    };
+	private wrapReceivedAndInvoke(callback: Function, notification: Notification) {
+		const completion = (response: NotificationCompletion) => {
+			if (Platform.OS === 'ios') {
+				this.nativeCommandsSender.finishPresentingNotification(
+					((notification as unknown) as NotificationIOS).identifier,
+					response
+				);
+			}
+		};
 
-    callback(notification, completion);
-  }
+		callback(notification, completion);
+	}
 
-  public wrapOpenedCallback(callback: Function): (notification: Notification) => void {
-    return (notification) => {
-      const completion = () => {
-        if (Platform.OS === 'ios') {
-          this.nativeCommandsSender.finishHandlingAction((notification as unknown as NotificationIOS).identifier);
-        }
-      };
+	public wrapOpenedCallback(
+		callback: Function
+	): (notification: Notification, action?: NotificationActionResponse) => void {
+		return (notification, action) => {
+			const completion = () => {
+				if (Platform.OS === 'ios') {
+					this.nativeCommandsSender.finishHandlingAction(
+						((notification as unknown) as NotificationIOS).identifier
+					);
+				}
+			};
 
-      callback(notification, completion);
-    }
-  }
+			callback(notification, completion, action);
+		};
+	}
 
-  private applicationIsVisible(): Boolean {
-    return AppState.currentState !== 'background';
-  }
+	private applicationIsVisible(): Boolean {
+		return AppState.currentState !== 'background';
+	}
 }
--- lib/src/events/EventsRegistry.ts
+++ lib/src/events/EventsRegistry.ts
@@ -1,37 +1,48 @@
 import { EmitterSubscription } from 'react-native';
 import { NativeEventsReceiver } from '../adapters/NativeEventsReceiver';
-import {
-  Registered,
-  RegistrationError,
-  NotificationResponse
-} from '../interfaces/NotificationEvents';
+import { Registered, RegistrationError } from '../interfaces/NotificationEvents';
 import { CompletionCallbackWrapper } from '../adapters/CompletionCallbackWrapper';
 import { Notification } from '../DTO/Notification';
 import { NotificationCompletion } from '../interfaces/NotificationCompletion';
+import { NotificationActionResponse } from '../interfaces/NotificationActionResponse';
 
 export class EventsRegistry {
-  constructor(
-    private nativeEventsReceiver: NativeEventsReceiver,
-    private completionCallbackWrapper: CompletionCallbackWrapper) 
-  {}
+	constructor(
+		private nativeEventsReceiver: NativeEventsReceiver,
+		private completionCallbackWrapper: CompletionCallbackWrapper
+	) {}
 
-  public registerRemoteNotificationsRegistered(callback: (event: Registered) => void): EmitterSubscription {
-    return this.nativeEventsReceiver.registerRemoteNotificationsRegistered(callback);
-  }
-  
-  public registerNotificationReceivedForeground(callback: (notification: Notification, completion: (response: NotificationCompletion) => void) => void): EmitterSubscription {
-    return this.nativeEventsReceiver.registerNotificationReceived(this.completionCallbackWrapper.wrapReceivedForegroundCallback(callback));
-  }
+	public registerRemoteNotificationsRegistered(callback: (event: Registered) => void): EmitterSubscription {
+		return this.nativeEventsReceiver.registerRemoteNotificationsRegistered(callback);
+	}
 
-  public registerNotificationReceivedBackground(callback: (notification: Notification, completion: (response: NotificationCompletion) => void) => void): EmitterSubscription {
-    return this.nativeEventsReceiver.registerNotificationReceived(this.completionCallbackWrapper.wrapReceivedBackgroundCallback(callback));
-  }
-  
-  public registerNotificationOpened(callback: (response: NotificationResponse, completion: () => void) => void): EmitterSubscription {
-    return this.nativeEventsReceiver.registerNotificationOpened(this.completionCallbackWrapper.wrapOpenedCallback(callback));
-  }
-  
-  public registerRemoteNotificationsRegistrationFailed(callback: (event: RegistrationError) => void): EmitterSubscription {
-    return this.nativeEventsReceiver.registerRemoteNotificationsRegistrationFailed(callback);
-  }
+	public registerNotificationReceivedForeground(
+		callback: (notification: Notification, completion: (response: NotificationCompletion) => void) => void
+	): EmitterSubscription {
+		return this.nativeEventsReceiver.registerNotificationReceived(
+			this.completionCallbackWrapper.wrapReceivedForegroundCallback(callback)
+		);
+	}
+
+	public registerNotificationReceivedBackground(
+		callback: (notification: Notification, completion: (response: NotificationCompletion) => void) => void
+	): EmitterSubscription {
+		return this.nativeEventsReceiver.registerNotificationReceived(
+			this.completionCallbackWrapper.wrapReceivedBackgroundCallback(callback)
+		);
+	}
+
+	public registerNotificationOpened(
+		callback: (notification: Notification, completion: () => void, action?: NotificationActionResponse) => void
+	): EmitterSubscription {
+		return this.nativeEventsReceiver.registerNotificationOpened(
+			this.completionCallbackWrapper.wrapOpenedCallback(callback)
+		);
+	}
+
+	public registerRemoteNotificationsRegistrationFailed(
+		callback: (event: RegistrationError) => void
+	): EmitterSubscription {
+		return this.nativeEventsReceiver.registerRemoteNotificationsRegistrationFailed(callback);
+	}
 }
